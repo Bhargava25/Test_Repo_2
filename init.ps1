883c41f231ef0fc7442845a448302de01ff44973
Bhargav <bharghavkaura@gmail.com>
2024-05-27 13:13:26 +0530

    Adding README.md and init.ps1 file

---
+++ init.ps1
@@ -0,0 +1,11 @@
+function Setup-Env {
+    Write-Host "Running pre-build init"
+}
+
+function Build-Proj {
+    Write-Host "Building Project"
+}
+
+function Release-Build {
+    Write-Host "Deploying Artifacts"
+}
